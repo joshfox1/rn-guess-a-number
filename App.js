7a13c2277182511dc3ca5329868c608fbe2256d8
joshfox1 <joshfox3@outlook.com>
2019-12-27 21:13:35 -0600

    created Header Component/ added Header to App.js

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import Header from "./Components/Header";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess A Number" />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  }
+});
